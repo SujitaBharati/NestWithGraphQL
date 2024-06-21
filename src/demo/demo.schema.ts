618b6066eab82d0a19e89bc182c03164191075b1
Sujita Bharati <Bharatisujita005@gmail.com>
2024-06-21 11:28:47 +0545

    change type

--- src/demo/demo.schema.ts
+++ src/demo/demo.schema.ts
@@ -2,8 +2,8 @@ import { Field, Int, ObjectType } from '@nestjs/graphql';
 
 @ObjectType()
 export class Author {
-  @Field((type) => Int)
-  id: number;
+  @Field((type) => String)
+  id: string;
 
   @Field({ nullable: true })
   firstName?: string;
