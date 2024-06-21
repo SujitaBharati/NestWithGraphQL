4effde66fb3399e53050c494e99c39927911fdbe
Sujita Bharati <Bharatisujita005@gmail.com>
2024-06-21 11:10:14 +0545

    feat:add necessary providers

--- src/demo/demo.module.ts
+++ src/demo/demo.module.ts
@@ -1,5 +1,18 @@
 import { Module } from '@nestjs/common';
 import { DemoResolver } from './demo.resolver';
+import { PubSub } from 'graphql-subscriptions';
+import { CqrsModule } from '@nestjs/cqrs';
+import { DemoCommandHandler } from './commands/demo.handler';
 
-@Module({ providers: [DemoResolver] })
+@Module({
+  imports: [CqrsModule],
+  providers: [
+    DemoResolver,
+    {
+      provide: 'PUB_SUB',
+      useValue: new PubSub(),
+    },
+    DemoCommandHandler,
+  ],
+})
 export class DemoModule {}
