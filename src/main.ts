91cf7fb6aa67b9b90fc133609421cc9e789676c6
Sujita Bharati <Bharatisujita005@gmail.com>
2024-06-21 11:18:29 +0545

    feat:add validationPipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {  ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      stopAtFirstError: true,
+      whitelist: true,
+    }),
+  );
+  await app.listen(3000, () => {
+   
+  });
+  // Start listening for incoming HTTP requests
 }
 bootstrap();
